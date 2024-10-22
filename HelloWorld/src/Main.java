71a43ac8cecf50828f69ab9bbd28f5dd54e91a6d
srinivas7989 <saikamsrinivas8@gmail.com>
2024-10-22 09:38:20 -0400

    Logical Operator

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,14 +2,30 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        boolean isAlien = false;
+        if(isAlien == true) System.out.println("It is an alien");
+        System.out.println("It is not an alien");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int topScore = 0;
+        if(topScore == 100){
+            System.out.println("you got high score!");
         }
+        if(topScore != 100){
+            System.out.println("you got high score!");
+        }
+        if(topScore < 100){
+            System.out.println("you got less score!");
+        }
+        if(topScore > 100){
+            System.out.println("you got highest score!");
+        }
+        int secoundTopScore = 60;
+        if(topScore < 100 && topScore == 0){
+            System.out.println("you got "+topScore);
+        }
+        if((topScore > 100) || (topScore == 0)){
+            System.out.println("you got "+topScore);
+        }
+
     }
 }
